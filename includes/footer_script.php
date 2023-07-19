b8fda4d2d68514eaee967e4657bf1629fd687b3c
Roovin Bansal <rovinbansal@gmail.com>
2023-07-19 16:14:35 +0530

    first commit

---
+++ includes/footer_script.php
@@ -0,0 +1,46 @@
+<script data-cfasync="false" src="assets/js/email-decode.min.js"></script>
+    <script src="assets/js/jquery.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="https://maps.google.com/maps/api/js?key=AIzaSyBjtXU1JtLBVn8-AA9ThYXvSl-ylfn5vEU"
+        type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/bootstrap.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/bootstrap-select.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/jquery.mmenu.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/jquery.inview.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/jquery.countdown.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/owl.carousel.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/owl.carousel.thumbs.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/isotope.pkgd.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/imagesloaded.pkgd.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/masonry.pkgd.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/wow.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/countup.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/moment.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/daterangepicker.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/parallax.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/smoothscroll.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/instafeed.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/main.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+
+    <script src="assets/revolution/js/jquery.themepunch.tools.min.js" type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/revolution/js/jquery.themepunch.revolution.min.js"
+        type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.actions.min.js"
+        type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.carousel.min.js"
+        type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.kenburn.min.js"
+        type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.layeranimation.min.js"
+        type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.migration.min.js"
+        type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.navigation.min.js"
+        type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.parallax.min.js"
+        type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.slideanims.min.js"
+        type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.video.min.js"
+        type="6f76b783a26ea8226eb3afcb-text/javascript"></script>
+    <script src="assets/js/rocket-loader.min.js" data-cf-settings="6f76b783a26ea8226eb3afcb-|49" defer></script>
---
+++ includes/head.php
@@ -0,0 +1,32 @@
+<head>
+    <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+
+    <title>Hotel Himara - Hotel HTML Template</title>
+    <meta content name="description">
+    <meta content name="keywords">
+    <meta content name="author">
+
+    <link rel="apple-touch-icon-precomposed" href="assets/images/favicon-apple.png" />
+    <link rel="icon" href="images/favicon.png">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="assets/css/jquery.mmenu.css">
+    <link rel="stylesheet" href="assets/revolution/css/layers.css">
+    <link rel="stylesheet" href="assets/revolution/css/settings.css">
+    <link rel="stylesheet" href="assets/revolution/css/navigation.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/daterangepicker.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <link href="assets/fonts/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/fonts/flaticon.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Oswald:400,500,600,700%7CRoboto:100,300,400,400i,500,700"
+        rel="stylesheet">
+</head>
